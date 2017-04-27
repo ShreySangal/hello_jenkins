7269c8870425f5cff73ce619b53e7a36613b2a37
AaronYang2333 <aaron19940628@gmail.com>
2017-04-27 15:22:08 +0800

    change the app's port which make the app become more acceptable

--- src/main/java/com/sight/jenkins/HelloJenkinsApplication.java
+++ src/main/java/com/sight/jenkins/HelloJenkinsApplication.java
@@ -2,11 +2,18 @@ package com.sight.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
 @SpringBootApplication
-public class HelloJenkinsApplication {
+public class HelloJenkinsApplication implements EmbeddedServletContainerCustomizer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloJenkinsApplication.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+		configurableEmbeddedServletContainer.setPort(8082);
 	}
 }
